02eeb47cddcf9420c906cb48f19897b89cd22e60
francoismavunila <mavunila002@gmail.com>
2024-05-19 17:04:23 +0000

    aded top nav responsiveness

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,16 +1,21 @@
-// components/Layout.js
+'use client'
 import Sidebar from "./components/Sidebar"
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { IoIosSearch } from "react-icons/io";
+import { useState } from 'react';
+import { IoIosMenu } from 'react-icons/io';
 
 export default function Layout({ children }) {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     return (
         <div className="flex">
-            <Sidebar />
+            {isSidebarOpen && <Sidebar onClose={() => setIsSidebarOpen(false)} />}
             <div className="flex flex-col w-full">
-                <nav className="bg-white text-black px-10 py-2 flex justify-between">
-                    <p className="text-3xl text-gray-600 font-bold">Welcome Mamoyo !!</p>
+                <nav className="bg-white text-black px-10 py-2 flex flex-col md:flex-row justify-between md:flex-row-reverse">
                     <div className="flex items-center gap-5">
+                    <button onClick={() => setIsSidebarOpen(!isSidebarOpen)} className="md:hidden">
+                        <IoIosMenu className="text-3xl text-gray-600" />
+                    </button>
                     <div className="relative">
                         <input className="border-2 border-gray-300 bg-white h-10 px-2 pr-8 rounded-full text-sm focus:outline-none"
                             type="search" name="search" placeholder="Search" />
@@ -18,6 +23,7 @@ export default function Layout({ children }) {
                     </div>
                      <IoIosNotificationsOutline className="text-2xl text-headercolor" />
                     </div>
+                    <p className="text-3xl text-gray-600 font-bold text-xl md:text-2xl">Welcome Mamoyo !!</p>
                 </nav>
                 <main className="px-10 py-2 bg-mainbg text-black flex-grow">
                     hsjghjgjhag
